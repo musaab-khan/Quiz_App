192819a704d6762b93c2659e61806ad3a6321c31
musaab-khan <musaabkhan4511@gmail.com>
2024-11-07 02:16:05 +0500

    Quiz Component Added
    TODO: Quiz Questions needs to be a component that will be added in Quiz component.

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
 export default function Quiz() {
+
+  function onNextClick(){
+    console.log("Next button clicked");
+  }
+  
+  function onPrevClick(){
+    console.log("Previous button clicked");
+  }
+
   return (
-    <div>Quiz</div>
+    <div className='container'>
+      <h1 className='title text-light'>Quiz Application</h1>
+      {/** Quiz Questions */}
+      <div className="grid">
+        <button className='btn prev' onClick={onNextClick}>Previous</button>
+        <button className='btn next' onClick={onPrevClick}>Next</button>
+      </div>
+    </div>
   )
 }
