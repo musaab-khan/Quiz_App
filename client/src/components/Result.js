df41f7a827d956f31a467330e2b64a2b19702614
musaab-khan <musaabkhan4511@gmail.com>
2024-11-07 23:51:55 +0500

    Result Component
    Added result component.

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Result.css';
+import { Link } from 'react-router-dom';
 
 export default function Result() {
   return (
-    <div>Result</div>
+    <div className='container'>
+      <h1 className='text-light title'>Quiz Result</h1>
+
+      <div className=" result flex-center">
+        <div className="flex">
+          <span>Username</span>
+          <span className="bold">Musaab Khan</span>
+        </div>
+        <div className="flex">
+          <span>Total Quiz Points</span>
+          <span className="bold">50</span>
+        </div>
+        <div className="flex">
+          <span>Total Attempts</span>
+          <span className="bold">03</span>
+        </div>
+        <div className="flex">
+          <span>Total Questions</span>
+          <span className="bold">15</span>
+        </div>
+        <div className="flex">
+          <span>Earned Points</span>
+          <span className="bold">45</span>
+        </div>
+        <div className="flex">
+          <span>Quiz Result</span>
+          <span className="bold">Passed</span>
+        </div>
+      </div>
+
+      <div className="start">
+        <Link to={'/'} className='btn'>Restart</Link>
+      </div>
+    </div>
   )
 }
